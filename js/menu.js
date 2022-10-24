c1aa818d73be922f9b170bfeec5a96f66bcaf090
mgutierrez <mgutierrez>
2022-10-24 17:22:24 -0300

    Se agrupan archivos .js en carpeta

--- js/menu.js
+++ js/menu.js
@@ -3,3 +3,7 @@ document.querySelector(".btn_menu").addEventListener("click", toggleMenu);
 function toggleMenu() {
     document.querySelector(".navigation").classList.toggle("show");
 }
+document.queryCommandIndeterm(params);
+function resert(){
+    document.querySelector(".btn_menu").addEventListener("click", toggleMenu);
+}
